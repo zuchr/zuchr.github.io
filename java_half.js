a694f6f9aa67c79d3d65100fa476adc23742266d
Bobby Zuch <bobbyzuch1@gmail.com>
2022-09-27 20:35:51 -0400

    Completely upgraded my portfolio!

--- java_half.js
+++ java_half.js
@@ -1,6 +1,6 @@
 function moveit() {
 	//document.getElementsByClassName('anim_shifting').style.animationDirection = "reverse";
-	var info = document.getElementById('backborder');
+	let info = document.getElementById('backborder');
 	info.style.animationName = "shift";
 	info.style.animationPlayState = "running";
 	info.style.animationTimingFunction = "ease-out";
@@ -9,8 +9,8 @@ function moveit() {
 }
 
 function moveitout() {
-	var info = document.getElementById('backborder');
-	var rect = info.getBoundingClientRect();
+	let info = document.getElementById('backborder');
+	let rect = info.getBoundingClientRect();
 	if ( rect.right < window.innerWidth ) { // Only plays exit animation if it's on-screen.
 		info.style.animationName = "shiftback";
 		info.style.animationPlayState = "running";
@@ -22,36 +22,63 @@ function moveitout() {
 
 //Clicking off the info box removes the hash.
 function closeit() {
-	history.pushState("", document.title, window.location.pathname + window.location.search); //Removes the hash while maintaining stuff like search parameters. Just in case I add search functionality in the future.
+	if (typeof searchstring !== 'undefined') {
+		hashname = "";
+		history.replaceState("", document.title, window.location.pathname + '#?' + searchstring); //Removes the hash while maintaining stuff. Only runs when on the main page.
+	}
 	moveitout();
 	galleryclose();
 }
 
 function about() { //Hardcoded entry exclusively for the "ABOUT" button.
+	let thisImageGetsReferenced16Times = document.getElementById("image");
+	thisImageGetsReferenced16Times.src = "https://cdn.discordapp.com/attachments/901206407695728660/919078708336271421/f39f1b_f8c96e98a63e4677bd0d251531026005mv2.png";
 	document.getElementById("title").innerHTML = "Hello & Welcome!";
-	document.getElementById("date").innerHTML = "<img src='../clock.svg'> " + "Site launched April 15th, 2022 (not publicly)";
-	document.getElementById("desc").innerHTML = "Welcome to my portfolio website! It's great to have you here. Included on this page is a sampling of some of my proudest and most recent works.<br><br>If you're an interested employer (or if you know a guy), <a href='https://d14d47d2-2c3f-4e01-abd9-ff667189580f.filesusr.com/ugd/f39f1b_230203c803fa48928b706826a4488ec9.pdf' target='_blank'>here is the latest version of my resume</a>.";
+	document.getElementById("date").innerHTML = "<img src='../clock.svg'> " + "Site launched September 26th, 2022";
+	document.getElementById("desc").innerHTML = "Welcome to my portfolio website! It's great to have you here. Included on this page is a sampling of some of my proudest and most recent works.<br><br>If you're an interested employer (or if you know a guy), <a href='../BobbyZuch_Resume_22-8-23.pdf' target='_blank'>here is the latest version of my resume</a>.";
 	document.getElementById("diff").innerHTML = "If you're looking to get in touch, shoot me an email at <a href='mailto:bobbyzuch1@gmail.com'>bobbyzuch1@gmail.com</a>. I'd love to hear from you!<br><br>I'm very proud to say I wrote this site by myself from the ground-up. That includes all the HTML, CSS, and Javascript. No bootstrap templates here!";
 	document.getElementById("skills").innerHTML = "<b>Skills:</b> " + "I make nice eggs.";
 	document.getElementById("software").innerHTML = "<b>Software:</b> " + "Anything I can get my dirty hands on!";
 	document.getElementById("client").innerHTML = "Thank you for visiting!";
+	document.getElementById("category").innerHTML = "Bobby Zuch!";
 	
 	//Checks if an iframe is currently being shown in the infobox.
+	
 	if ( document.getElementById("iframeouter")) {
 		document.getElementById("iframeouter").style.display = "none";
 		document.getElementById("iframeA").src = "";
-		document.getElementById("image").style.display = "block";
+		thisImageGetsReferenced16Times.style.display = "block";
+		thisImageGetsReferenced16Times.removeAttribute("onclick");
+		thisImageGetsReferenced16Times.style.cursor = "";
+		thisImageGetsReferenced16Times.parentElement.removeAttribute("class");
+		document.getElementById("extra").style.display = "none";
 	}
-	document.getElementById("image").src = "https://cdn.discordapp.com/attachments/901206407695728660/919078708336271421/f39f1b_f8c96e98a63e4677bd0d251531026005mv2.png";
 	
 	closeit();
 	moveit();
 }
 
 function galleryclose() {
-	var grayback = document.getElementById("grayback");
-	var closeX2 = document.getElementById("closeX2");
+	let grayback = document.getElementById("grayback");
+	let closeX2 = document.getElementById("closeX2");
 	grayback.style.display = "none";
 	grayback.innerHTML = ("<div id='closeX2' onclick='closeit()'><h1>X</h1></div>");
 	closeX2.style.display = "none";
 }
+
+let touchstartX = 0;
+let touchendX = 0;
+    
+function checkDirection() {
+	if (touchendX < touchstartX && (touchendX - touchstartX) < -105) { nextprev(1); }
+	if (touchendX > touchstartX && (touchendX - touchstartX) > 105) { nextprev(0); }
+}
+
+document.addEventListener('touchstart', e => {
+	touchstartX = e.changedTouches[0].screenX;
+});
+
+document.addEventListener('touchend', e => {
+	touchendX = e.changedTouches[0].screenX;
+	checkDirection();
+});
--- main.js
+++ main.js
@@ -1,29 +1,113 @@
-var currentrow = 0; //Set to a number to test that row from the TSV.
-var filled = 0; //For filling the gallery images only once.
-var data;
+let currentrow = 0; //Set to a number to test that row from the TSV.
+let filled = 0; //For filling the gallery images only once.
+let data = [];
+let dataOG; //So we can remove stuff from 'data' while having this as backup.
+let hashname; // Moved here for referencing from getfromsearch().
+let cachedsearch; // To not reload the search when it's the same as before.
 
 function loaddata() {
-	d3.tsv("./Portfolio_Content.tsv").then(copythisdata => {
-		data = copythisdata;
+	d3.tsv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSsVi4ZkLBQI9OEiEtKEzE9vMGW7Uwu3Wuu60wBgxUAm7uXd16KqCgSLGWwpRbGf6-4YMRpyYLNqjxv/pub?output=tsv").then(copythisdata => {
+		dataOG = copythisdata;
+		//data = dataOG;
 		loadfromhash();
 	});
 }
 
+function narrowdata() {
+	if ( filled == 1 ) {
+		let gallery = document.getElementById("gallery");
+		data = [];
+		
+		gallery.innerHTML = "<h1 id='getridofme'>Loading page content...</h1><noscript><h1 style='text-align: center'>This page needs Javascript<br>enabled to function properly.</h1></noscript>";
+		
+		//Remove all but the h1 and noscript, make loading text visible again before starting.
+		document.getElementById("getridofme").style.display = "unset";
+		
+		/*for (let i = 2; i < (gallery.children.length); i++) { //Starts at 2 to avoid the non-gallery objects.
+			console.log("removing " + gallery.children[i]);
+			gallery.children[i].remove();
+		}*/
+		filled = 0;
+	}
+	
+	//data = dataOG;
+	
+	if ( found == 1 && anysearches == 1 && searchstring !== '' && searchstring !== 'All' && searchstring !== 'Featured' ) {
+		// If it's one of the buttons and a non-empty search was performed...
+		//console.log("glung");
+		for (let i = 0; i <= (dataOG.length - 1); i++) {
+			if ( dataOG[i].Category === searchstring ) {
+				data.push(dataOG[i]);
+			}
+		}
+	}
+	else if ( searchstring === 'Featured' ) {
+		//console.log("granngle");
+		for (let i = 0; i <= (dataOG.length - 1); i++) {
+			for (let o = 0; o <= (dataOG.length - 1); o++) {
+				if ( dataOG[o].Featured == [i + 1] ) {
+					data.push(dataOG[o]);
+					break;
+				}
+			}
+		}
+	}
+	else if ( found == 0 && anysearches == 1 && searchstring !== '' && searchstring !== 'All' ) {
+		// If it's NOT one of the buttons and a non-empty search was performed...
+		//console.log("grearching");
+
+		//let searchableColumns = ["Title", "Info", "Difficulties", "Skills", "Software", "Client", "Category", "Permahash"];
+		
+		for (let i = 0; i <= (dataOG.length - 1); i++) {
+				if (
+					dataOG[i].Title.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Info.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Difficulties.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Skills.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Software.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Client.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Category.toLowerCase().includes(searchstring.toLowerCase()) ||
+					dataOG[i].Permahash.toLowerCase().includes(searchstring.toLowerCase())
+				) {
+					data.push(dataOG[i]);
+				}
+		}
+	}
+	else {
+		data = dataOG;
+		//console.log("jungle");
+	}
+	
+	if (data.length == 0) {
+		gallery.innerHTML = "<h2 style='color: white; opacity: 50%; text-align: center; width: 470px;'>Nothing came up from your search...<br><br>(I'm still migrating content over from my scrappy <a href='https://1yoshibobby1.wixsite.com/bobbyzuch/' target='_blank'>old portfolio</a>, so missing content might be there.)</h2><h1 id='getridofme'>Loading page content...</h1><noscript><h1 style='text-align: center'>This page needs Javascript<br>enabled to function properly.</h1></noscript>";
+	}
+}
+
 function makehash(currentpermahash) { //Makes a hash out of the value given by the clicked HTML element.
+	if ( anysearches == 1 ) { currentpermahash = currentpermahash + '?' + searchstring; }
 	location.hash = currentpermahash;
 }
 
-function loadfromhash() { //Performs actions based around the number of the hash.	
-	var hashname = location.hash.substring(1); //This removes the '#' from the hash referencer.
-	var parsin = 0;
+function loadfromhash() { //Performs actions based around the number of the hash.
+	//console.log("loading...");
+	if ( anysearches == 1 ) {
+		hashname = location.hash.slice(0, location.hash.indexOf('?')).substring(1);
+	}
+	else { hashname = location.hash.substring(1); }
+	// ^ This removes the '#' from the hash referencer, and removes the search string.
+	
+	getfromsearch();
+	if ( cachedsearch !== searchstring ) { narrowdata(); }
+	
+	let parsin = 0;
 	while (parsin <= (data.length - 1) && (data[parsin].Permahash != hashname)) { parsin++; }
 	currentrow = parsin;
 
 	if( filled == 0 ) { //Populates the main gallery on the initial run.
-		var gallery = document.getElementById("gallery");
+		let gallery = document.getElementById("gallery");
 		
 		for (let i = 0; i <= (data.length - 1); i++) {
-			var x = document.createElement("IMG");
+			let x = document.createElement("IMG");
 			x.setAttribute("id", "imgnum" + i);
 			x.setAttribute("onclick", "makehash('" + data[i].Permahash + "')");
 			x.setAttribute("alt", data[i].Title);
@@ -36,37 +120,61 @@ function loadfromhash() { //Performs actions based around the number of the hash
 	}
 	//The above code populates the main page with the thumbnail images from the TSV if they haven't been loaded already. It even creates the HTML elements as well!
 	//Infobox populating code below:
-	if (location.hash != "") { // Removes error message on blank initial load.
-	document.getElementById("image").src = ""; //Quickly clear the last image before it slides in.
+	if (hashname != "") { // Removes error message on blank initial load.
+	let thisImageGetsReferenced16Times = document.getElementById("image");
+	thisImageGetsReferenced16Times.src = ""; //Quickly clear the last image before it slides in.
 	document.getElementById("title").innerHTML = (data[currentrow].Title);
-	document.getElementById("date").innerHTML = "<img src='./clock.svg'> " + (data[currentrow].Date);
+	document.getElementById("date").innerHTML = "<img src='../clock.svg'> " + (data[currentrow].Date);
 	document.getElementById("desc").innerHTML = (data[currentrow].Info);
 	document.getElementById("diff").innerHTML = (data[currentrow].Difficulties);
 	document.getElementById("skills").innerHTML = "<b>Skills:</b> " + (data[currentrow].Skills);
 	document.getElementById("software").innerHTML = "<b>Software:</b> " + (data[currentrow].Software);
 	document.getElementById("client").innerHTML = (data[currentrow].Client);
+	document.getElementById("category").innerHTML = (data[currentrow].Category);
 	if (data[currentrow].Type === "img") {
-		console.log("imgtime");
+		//console.log("imgtime");
 		document.getElementById("iframeouter").style.display = "none";
 		document.getElementById("iframeA").src = "";
-		document.getElementById("image").style.display = "block";
-		document.getElementById("image").src = (data[currentrow].URL); }
+		thisImageGetsReferenced16Times.style.display = "block";
+		thisImageGetsReferenced16Times.src = (data[currentrow].URL);
+		thisImageGetsReferenced16Times.removeAttribute("onclick");
+		thisImageGetsReferenced16Times.style.cursor = "";
+		thisImageGetsReferenced16Times.parentElement.removeAttribute("class");
+		document.getElementById("extra").style.display = "none"; }
+		
 	else if (data[currentrow].Type === "iframe") {
-		console.log("iframetime");
+		//console.log("iframetime");
 		document.getElementById("getridofme2").style.display = "block"; //Will turn back to none after loading.
-		document.getElementById("image").style.display = "none";
-		document.getElementById("image").src = "";
-		document.getElementById("iframeouter").style.display = "block";
-		document.getElementById("iframeA").src = (data[currentrow].URL); }
+		thisImageGetsReferenced16Times.style.display = "none";
+		thisImageGetsReferenced16Times.src = "";
+		thisImageGetsReferenced16Times.removeAttribute("onclick");
+		thisImageGetsReferenced16Times.style.cursor = "";
+		document.getElementById("iframeouter").style.display = "flex";
+		document.getElementById("iframeA").src = (data[currentrow].URL);
+		thisImageGetsReferenced16Times.parentElement.removeAttribute("class");
+		document.getElementById("extra").style.display = "none"; }
+		
+	else { // When the Type column contains something else, like a URL, link to it:
+		//console.log("clickabletime");
+		document.getElementById("iframeouter").style.display = "none";
+		document.getElementById("iframeA").src = "";
+		thisImageGetsReferenced16Times.style.display = "block";
+		thisImageGetsReferenced16Times.src = (data[currentrow].URL);
+		thisImageGetsReferenced16Times.setAttribute("onclick", "openthis('" + data[currentrow].Type + "')");
+		thisImageGetsReferenced16Times.style.cursor = "pointer";
+		thisImageGetsReferenced16Times.parentElement.setAttribute("class", "extraroom");
+		document.getElementById("extra").style.display = "block"; }
 	
 	moveit();
-	//hidenavbuttons();
+	hidenavbuttons();
 	}
+	
+	cachedsearch = searchstring;
 }
 
 document.addEventListener('keydown', function(event) {
-	var info = document.getElementById('backborder');
-	var rect = info.getBoundingClientRect();
+	let info = document.getElementById('backborder');
+	let rect = info.getBoundingClientRect();
 	if ( rect.right < window.innerWidth ) {
 		const key = event.key;
 		switch (event.key) {
@@ -76,11 +184,13 @@ document.addEventListener('keydown', function(event) {
 	}
 });
 
+function openthis(link) { window.open(link, "_blank"); }
+
 function nextprev(direction) {
 	if (direction == 1 && currentrow < data.length - 1) { makehash(data[currentrow + 1].Permahash); }
 	else if (direction == 0 && currentrow > 0) { makehash(data[currentrow - 1].Permahash); }
 	
-	//hidenavbuttons();
+	hidenavbuttons();
 }
 
 function hidenavbuttons() {
